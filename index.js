37bc5ab2daf7e8c2fb7efa4fcc45241bd0020597
ChristophyBrunkenstein <86986214+ChristophyBrunkenstein@users.noreply.github.com>
2021-07-06 10:54:38 -0700

    QoL improvements
    Upgrades

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ slamAudio = document.getElementById("slamAudio");
 linkNav = document.querySelector(".center");
 circleContainer = document.querySelector(".circle-container");
 
+
+
 slamBtn.addEventListener("click", function(){
+    slamAudio.volume = 0.4;
     slamAudio.play();
     console.log("clicked");
     circleContainer.classList.add("dance");
@@ -12,7 +15,7 @@ slamBtn.addEventListener("click", function(){
 
 function delayNavigate(e) {
     e.preventDefault();                
-    const goTo = this.getAttribute('/C:/Users/C_Bro/Documents/Learning/website/www/home.html'); // store anchor href
+    const goTo = this.getAttribute('/C:/Users/C_Bro/Documents/Learning/website/www/home.html');
   
     setTimeout(function(){
       window.location.href = slamLink;
--- www/home.js
+++ www/home.js
@@ -3,7 +3,10 @@
 // function scrollFunction() {
 //   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
 //     document.getElementById("header").style.padding = "1px";
+//     document.getElementById("header").style.fontSize = "12px";
 //   } else {
 //     document.getElementById("header").style.padding = "8px";
+//     document.getElementById("header").style.fontSize = "16px";
 //   }
 // }
+
