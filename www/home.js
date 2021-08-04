d197fa7da524ecb377267cb72bd76478976e93c5
ChristophyBrunkenstein <86986214+ChristophyBrunkenstein@users.noreply.github.com>
2021-08-04 12:06:14 -0700

    Updates
    Updates to webzone have been made

--- www/home.js
+++ www/home.js
@@ -1,3 +1,11 @@
+mainHeader0 = document.getElementById("mainHeader0");
+mainHeader1 = document.getElementById("mainHeader1");
+mainHeader2 = document.getElementById("mainHeader2");
+mainHeader3 = document.getElementById("mainHeader3");
+text0 = document.getElementById("text0");
+text1 = document.getElementById("text1");
+text2 = document.getElementById("text2");
+text3 = document.getElementById("text3");
 // window.onscroll = function() {scrollFunction()};
 // var element =  document.getElementById("header");
 // function scrollFunction() {
@@ -10,3 +18,9 @@
 //   }
 // }
 
+let randNum = Math.floor(Math.random() * 4);
+console.log(randNum);
+if(randNum === 0){ mainHeader0.style.display = "block"; text0.style.display = "block"}
+if(randNum === 1){ mainHeader1.style.display = "block"; text1.style.display = "block"}
+if(randNum === 2){ mainHeader2.style.display = "block"; text2.style.display = "block"}
+if(randNum === 3){ mainHeader3.style.display = "block"; text3.style.display = "block"}
